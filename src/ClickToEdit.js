fb72816aab5a4c4f5149cdda5bf1b262d2a31aa6
Sebastian Frysztak <sebfry@gmail.com>
2018-11-16 23:24:12 +0100

    Markdown: working click-to-tick-checkbox

--- src/ClickToEdit.js
+++ src/ClickToEdit.js
@@ -8,15 +8,38 @@ class ClickToEdit extends React.Component {
     this.handleTextChange = this.handleTextChange.bind(this);
     this.onClicked = this.onClicked.bind(this);
     this.onEditorLostFocus = this.onEditorLostFocus.bind(this);
+    this.onCheckboxClicked = this.onCheckboxClicked.bind(this);
 
     this.state = {
       value: props.text,
       showEditor: false
     };
+
+    this.renderers = {
+      listItem: this.listItemRenderer.bind(this)
+    };
   }
 
-  handleTextChange(e) {
-    const text = e.target.value;
+  listItemRenderer(props) {
+    let checkbox = null;
+    if (props.checked !== null) {
+      checkbox = (
+        <input type="checkbox" checked={props.checked} readOnly={true} />
+      );
+    }
+
+    return (
+      <li
+        onClick={this.onCheckboxClicked}
+        data-sourcepos={props["data-sourcepos"]}
+      >
+        {checkbox}
+        {props.children}
+      </li>
+    );
+  }
+
+  handleTextChange(text) {
     this.props.onTextChange(text);
     this.setState({ value: text });
   }
@@ -27,6 +50,22 @@ class ClickToEdit extends React.Component {
 
   onClicked() {
     this.setState({ showEditor: true });
+  }
+
+  onCheckboxClicked(e) {
+    e.stopPropagation();
+    const sourcepos = e.currentTarget.getAttribute("data-sourcepos");
+    this.tickCheckbox(sourcepos);
+  }
+
+  tickCheckbox(sourcepos) {
+    const beginning = sourcepos.split("-")[0];
+    let line = beginning.split(":")[0];
+    line -= 1; // sourcepos indexing starts with 1
+
+    let lines = this.state.value.split("\n");
+    lines[line] = lines[line].replace("[ ]", "[x]");
+    this.handleTextChange(lines.join("\n"));
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
@@ -46,11 +85,15 @@ class ClickToEdit extends React.Component {
             cols="50"
             autoFocus={true}
             onBlur={this.onEditorLostFocus}
-            onChange={this.handleTextChange}
+            onChange={e => this.handleTextChange(e.target.value)}
             value={this.state.value}
           />
         ) : (
-          <ReactMarkdown source={this.state.value} />
+          <ReactMarkdown
+            source={this.state.value}
+            renderers={this.renderers}
+            sourcePos={true}
+          />
         )}
       </div>
     );
